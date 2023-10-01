b98d97c1fa0d5fbc5ebb95878f1865c185334b76
Tanmai Kamat <tanmaikamat@gmail.com>
2023-10-02 01:09:04 +0530

    Auth decorator for PMP User Added

---
+++ server/pmp_auth/decorators.py
@@ -0,0 +1,68 @@
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from utils.auth import verify_token as jwt_verify
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def auth_required(function):
+    @csrf_exempt
+    def wrapper(request,*args,**kwargs):
+        token=None
+        user=None
+        try:
+                token=request.headers["Authorization"]
+            
+        except KeyError:
+            return JsonResponse(status=403,data={"message":"Token missing. Please add Token to header as Authorization : Bearer ..."})
+        try:
+            user=request.GET["user"]
+        except KeyError:
+            return JsonResponse(status=400,data={"message":"User missing. Please add a parameter as user=user_id"})
+        except Exception as e:
+            print(e)
+            return JsonResponse(status=400,data={"message":"User missing. Please add a parameter as user=user_id"})
+        # token=request.headers["Authorization"]
+        # user=request.GET["user"]
+        request.pmp_user=user
+        request.is_admin=False
+        authorised=jwt_verify(token,user)
+        if(authorised):
+            return function(request,*args,**kwargs)
+        return JsonResponse({"status_code": 403,"status_msg":"Invalid Token"},status=403, safe=False)
+    return wrapper
+#TODO: Add admin_required decorator
+
+def admin_required(function):
+    def wrapper(request,*args,**kwargs):
+        token=None
+        user=None
+        try:
+            token=request.headers["Authorization"]
+        except KeyError:
+            return JsonResponse(status=403,data={"message":"Token missing. Please add Token to header as Authorization : Bearer ..."})
+        try:
+            user=request.GET["user"]
+        except KeyError:
+            return JsonResponse(status=400,data={"message":"User missing. Please add a parameter as user=user_id"})
+        except Exception as e:
+            print(e)
+            return JsonResponse(status=400,data={"message":"User missing. Please add a parameter as user=user_id"})
+        token=request.headers["Authorization"]
+        user=request.GET["user"]
+
+        if(token==None):
+            return JsonResponse(status=403,data={"message":"Token missing. Please add Token to header as Authorization : Bearer ..."})
+        if(user==None):
+            return JsonResponse(status=400,data={"message":"User missing. Please add a parameter as user=user_id"})
+        request.user=user
+        request.is_admin=False
+        authorised=jwt_verify(token,user)
+        if(authorised):
+            return function(request,*args,**kwargs)
+        return JsonResponse({"status_code": 403,"status_msg":"Invalid Token"},status=403, safe=False)
+    return wrapper
+
+
+
+
+
