91f830d74007cabbabfd8b56ac096ba17e56a911
Tanmai Kamat <tanmaikamat@gmail.com>
2024-03-11 16:07:06 +0530

    Bug Fix

--- server/pmp_user/urls.py
+++ server/pmp_user/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('users/list', views.list_users),
     path('users/create', views.UserListCreateView.as_view(), name='pmp-create-users'),
     path('users/<str:pk>/delete/', views.UserRetrieveUpdateDestroyView.as_view(), name='pmp-delete-user'),
+    path('user/delete_user/admin_rights/',views.delete_all_users)
 
 ]
 
